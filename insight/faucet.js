b8a81b9730670cefbbbb45928c2e3d0918abe4f7
Toshihide Hara <toshihidehr@gmail.com>
2018-09-07 10:52:17 +0900

    Fix faucet.

--- insight/faucet.js
+++ insight/faucet.js
@@ -18,6 +18,25 @@ schedule.scheduleJob(`*/${UPDATE_INTERVAL_SEC} * * * * *`, function() {
   });
 });
 
+// proxy
+var proxy = require('http-proxy-middleware');
+var options = {
+  target: `http://localhost:${PORT_INSIGHT}`,
+  changeOrigin: true,
+  router: {
+    [`localhost:${PORT_FAUCET}`]: `http://localhost:${PORT_INSIGHT}`,  // insight ui
+    [`localhost:${PORT_FAUCET}/api`]: `http://localhost:${PORT_INSIGHT}/api`  // insight api
+  }
+};
+// Node: The http-proxy-middleware should be above the body-parser https://github.com/chimurai/http-proxy-middleware/issues/40
+app.use('/', proxy(options));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
+
+// faucet api
 app.post('/faucet/:address', (req, res) => {
   const address = req.params.address;
   const amount = req.body.amount || 0;
@@ -46,24 +65,6 @@ app.post('/faucet/:address', (req, res) => {
 app.listen(PORT_FAUCET, () => {
     console.log('Faucet server up!');
 });
-
-// proxy
-var proxy = require('http-proxy-middleware');
-var options = {
-  target: `http://localhost:${PORT_INSIGHT}`,
-  changeOrigin: true,
-  router: {
-    [`localhost:${PORT_FAUCET}`]: `http://localhost:${PORT_INSIGHT}`,  // insight ui
-    [`localhost:${PORT_FAUCET}/api`]: `http://localhost:${PORT_INSIGHT}/api`  // insight api
-  }
-};
-app.use('/', proxy(options));
-
-// Node: The http-proxy-middleware should be above the body-parser https://github.com/chimurai/http-proxy-middleware/issues/40
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-// parse application/json
-app.use(bodyParser.json());
 
 // run bitcore server
 // reference: https://github.com/bitpay/bitcore/blob/master/bin/bitcored
